66894bfbdff2832e4512de448dcd9e25217e0fd9
signorettif <signorettif@users.noreply.github.com>
2018-10-07 16:34:10 +0200

    Delete dev.js

--- src/config/dev.js
+++
@@ -1,7 +0,0 @@
-// This file contains the actual strings
-
-export const FirebaseConfig = {
-  apiKey: "AIzaSyAbRdERQghkKH4zWBYPmx1ot5H7Q9BORbs",
-  authDomain: "tentativo-42f71.firebaseapp.com",
-  databaseURL: "https://tentativo-42f71.firebaseio.com"
-};

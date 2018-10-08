1a83a82d03acc8e5e028e6ea806d8f8ddc606284
lorenzosignoretti <lorenzosigno@gmail.com>
2018-10-08 21:40:00 +0100

    Fatto un poco della pagina singola, devo sistemare lo styles che non me lo prende

--- src/components/OfferPage.js
+++ src/components/OfferPage.js
@@ -4,8 +4,22 @@ import { compose } from "redux";
 import _ from "lodash";
 import * as actions from "../actions";
 
-import { Paper, Typography } from '@material-ui/core';
-import "../styles/offers.scss"
+import withStyles from '@material-ui/core/styles/withStyles';
+import { CssBaseline, Paper, Typography, List, ListItemText, ListItem } from '@material-ui/core';
+import "../styles/offerPage.scss"
+
+const styles = theme => ({
+  paper: {
+      marginTop: theme.spacing.unit * 3,
+      marginBottom: theme.spacing.unit * 3,
+      padding: theme.spacing.unit * 2,
+      [theme.breakpoints.up(600 + theme.spacing.unit * 3 * 2)]: {
+        marginTop: theme.spacing.unit * 6,
+        marginBottom: theme.spacing.unit * 6,
+        padding: theme.spacing.unit * 3,
+      },
+    }
+});
 
 class OfferPage extends Component {
 
@@ -14,23 +28,24 @@ class OfferPage extends Component {
     this.props.getOfferById(offerId)
   }
 
-
-  componentDidMount() {
-
-  }
-
   render() {
     const { data } = this.props;
 
     return(
+      <React.Fragment>
+          <CssBaseline />
           <Paper>
-            <Typography variant="h1">
+            <Typography variant="h4" align="center">
               {data.title}
-              {data.description}
-              {data.discountedAmount}
-              {data.originalAmount}
             </Typography>
+            <List disablePadding>
+              <ListItem>
+                <ListItemText primary={data.description} />
+                <ListItemText primary={data.discountedAmount} secondary={data.originalAmount}/>
+              </ListItem>
+            </List>
         </Paper>
+      </React.Fragment>
     )
 
   }
@@ -43,5 +58,6 @@ const mapStateToProps = ({ data }) => {
 };
 
 export default compose(
+  withStyles(styles),
   connect(mapStateToProps, actions)
 )(OfferPage)

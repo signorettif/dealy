631d8257a7892d447bf44bc354b4fb375ac39119
lorenzosignoretti <lorenzosigno@gmail.com>
2018-10-18 19:53:59 +0100

    Miglioramenti homepage

--- src/components/OffertaSingola.js
+++ src/components/OffertaSingola.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { NavLink } from 'react-router-dom';
-import { compose } from "redux";
+import classNames from 'classnames/bind';
 import _ from "lodash";
 import * as actions from "../actions";
 
@@ -14,14 +14,24 @@ class OffertaSingola extends Component {
   render() {
     const { offerId, offer } = this.props;
 
+    var hotStatus = classNames({
+      'isHot': offer.heatCount > 2,
+      'isCold': offer.heatCount < 2
+    });
+
     return(
       <NavLink to={"/offer/" + offerId } style={{ textDecoration: 'none', color: 'unset' }}>
         <div className="carta-offerta" fullWidth>
-            <Typography variant="h4" className="hot-score">{offer.heatCount}</Typography>
-            <Typography variant="body2" className="titolo-offerta">
-              {offer.title}
-            </Typography>
-            <div> </div>
+          <div className={"heat-count-circle " + hotStatus}>
+            {offer.heatCount}
+          </div>
+          <Typography variant="body2" className="titolo-offerta">
+            {offer.title}
+          </Typography>
+          <div className="prezzi">
+            <span className="originale" visibility={(offer.originalAmount=="") ? "visible" : "hidden"}> {offer.originalAmount} € </span>
+            <span className="scontato"> {offer.discountedAmount} € </span>
+          </div>
 
         </div>
       </NavLink>

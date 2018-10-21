5a88940363b556a56e4b6c572c14696c7f71382c
lorenzosignoretti <lorenzosigno@gmail.com>
2018-10-21 18:14:09 +0100

    aggiunti bottoni up e down

--- frontend/src/components/OffertaSingola.js
+++ frontend/src/components/OffertaSingola.js
@@ -8,7 +8,8 @@ import Api from '../Api';
 import Store from '../Store';
 
 // Style
-import { Typography } from '@material-ui/core';
+import { Button, Typography } from '@material-ui/core';
+import { KeyboardArrowUp, KeyboardArrowDown } from '@material-ui/icons';
 import "../styles/offertaSingola.scss"
 
 class OffertaSingola extends Component {
@@ -26,40 +27,55 @@ class OffertaSingola extends Component {
   render() {
     const { offer } = this.props;
 
-
+    //Class to algorithmically assign the heat status to the offer
     var hotStatus = classNames({
       'is-hot': offer.heatCount > 100,
       'is-cold': offer.heatCount < -100
     });
 
     return(
-      <NavLink to={"/offer/" + offer.id } style={{ textDecoration: 'none', color: 'unset' }}>
-        <div className={"carta-offerta " + hotStatus}>
-          {(offer.downloadURL) ?
-            <div className="immagine-offerta">
-              <div style= {{backgroundImage: 'url(' + offer.downloadURL + ')'}} className="img-circle" />
-            </div>
-          : null}
-          <div className = 'main-content'>
-            <Typography className="titolo-offerta">
-              {offer.title}
-            </Typography>
-            <Typography className="luogo-offerta">
-              {offer.vendor}
-            </Typography>
-          </div>
-
-          <div className='right-info'>
-            <div className="prezzi">
-              {(offer.originalAmount) ? <span className="originale"> {offer.originalAmount}€</span> : null}
-              <span className="scontato"> {offer.discountedAmount}€</span>
-            </div>
-            <div className='heat-count'>
-              {offer.heatCount}°
-            </div>
-          </div>
+      <div className="bottoni-farlocco">
+        <div className="bottoni-container">
+          <Button
+            className="{classes.floatButton}"
+          >
+            <KeyboardArrowUp />
+          </Button>
+          <Button
+            className="{classes.floatButton}"
+          >
+            <KeyboardArrowDown />
+          </Button>
         </div>
-      </NavLink>
+
+        <NavLink to={"/offer/" + offer.id } style={{ textDecoration: 'none', color: 'unset' }}>
+          <div className={"carta-offerta " + hotStatus}>
+            {(offer.downloadURL) ?
+              <div className="immagine-offerta">
+                <div style= {{backgroundImage: 'url(' + offer.downloadURL + ')'}} className="img-circle" />
+              </div>
+            : null}
+            <div className = 'main-content'>
+              <Typography className="titolo-offerta">
+                {offer.title}
+              </Typography>
+              <Typography className="luogo-offerta">
+                {offer.vendor}
+              </Typography>
+            </div>
+
+            <div className='right-info'>
+              <div className="prezzi">
+                {(offer.originalAmount) ? <span className="originale"> {offer.originalAmount}€</span> : null}
+                <span className="scontato"> {offer.discountedAmount}€</span>
+              </div>
+              <div className='heat-count'>
+                {offer.heatCount}°
+              </div>
+            </div>
+          </div>
+        </NavLink>
+      </div>
     )
 
   }
